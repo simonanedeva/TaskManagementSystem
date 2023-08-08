5ced9f5b9ce4e57a19b6cdeb28c211cd2cb299a4
simonanedeva <simona.nedeva2@gmail.com>
2023-08-08 17:30:55 +0300

    Implementation of Board interface and BoardImpl class.

--- TaskManagementSystem/src/com/company/oop/taskManagementSystem/models/BoardImpl.java
+++ TaskManagementSystem/src/com/company/oop/taskManagementSystem/models/BoardImpl.java
@@ -1,6 +1,47 @@
 package com.company.oop.taskManagementSystem.models;
 
+import com.company.oop.taskManagementSystem.models.contracts.ActivityLog;
 import com.company.oop.taskManagementSystem.models.contracts.Board;
+import com.company.oop.taskManagementSystem.models.contracts.Member;
+import com.company.oop.taskManagementSystem.models.contracts.Task;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BoardImpl implements Board {
+
+    private String name;
+    private final List<Task> tasks;
+    private final List<ActivityLog> activityHistory;
+
+    public BoardImpl(String name) {
+        setName(name);
+        tasks = new ArrayList<>();
+        activityHistory = new ArrayList<>();
+    }
+
+    private void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public List<Task> getTasks() {
+        return new ArrayList<>(tasks);
+    }
+
+    @Override
+    public List<ActivityLog> getActivityHistory() {
+        return new ArrayList<>(activityHistory);
+    }
+
+    public void addTask(Task task) {
+        tasks.add(task);
+    }
+
+
 }
--- TaskManagementSystem/src/com/company/oop/taskManagementSystem/models/contracts/Board.java
+++ TaskManagementSystem/src/com/company/oop/taskManagementSystem/models/contracts/Board.java
@@ -1,4 +1,12 @@
 package com.company.oop.taskManagementSystem.models.contracts;
 
+import java.util.List;
+
 public interface Board {
+
+    public String getName();
+
+    public List<Task> getTasks();
+
+    public List<ActivityLog> getActivityHistory();
 }
