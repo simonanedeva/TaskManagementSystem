55fdf6efbc2e78265355fe486360c6f165169a32
simonanedeva <simona.nedeva2@gmail.com>
2023-08-15 18:10:38 +0300

    Minor bug in the Assign command fixed. To be further optimized.

--- TaskManagementSystem/src/com/company/oop/taskManagementSystem/commands/AssignStoryCommand.java
+++ TaskManagementSystem/src/com/company/oop/taskManagementSystem/commands/AssignStoryCommand.java
@@ -10,6 +10,7 @@ public class AssignStoryCommand extends BaseCommand {
 
     private static final String TASK_REASSIGNED_SUCCESSFULLY = "Assignee of story %s successfully reassigned from %s to %s!";
     public static final int EXPECTED_NUMBER_OF_ARGUMENTS = 2;
+    public static final String ASSIGNEE_ERR_MESSAGE = "Assignee %s is not part of team %s!";
 
     public AssignStoryCommand(TMSRepository tmsRepository) {
         super(tmsRepository);
@@ -26,6 +27,7 @@ public class AssignStoryCommand extends BaseCommand {
     private String reassignStory(String storyToReassign, String newAssignee) {
         Member member = getTmsRepository().getLoggedInMember();
         Team memberTeam = getTmsRepository().findTeamOfMember(member.getUsername());
+        List<Member> membersInTeam = memberTeam.getMembers();
         List<Board> boardsList = memberTeam.getBoards();
 
         for (Board board : boardsList) {
@@ -33,6 +35,7 @@ public class AssignStoryCommand extends BaseCommand {
             for (Story story : stories) {
                 if (story.getTitle().equals(storyToReassign)) {
                     Member oldAssignee = getTmsRepository().findMemberByUsername(story.getAssignee());
+                    throwIfInvalidAssignee(newAssignee, memberTeam, membersInTeam);
                     story.changeAssignee(newAssignee);
                     story.logEvent(String.format("%s changed the assignee of story %s from %s to %s.", member.getUsername(), story.getTitle(), oldAssignee.getUsername(), newAssignee));
                     member.logEvent(String.format("%s changed the assignee of story %s from %s to %s.", member.getUsername(), story.getTitle(), oldAssignee.getUsername(), newAssignee));
@@ -41,6 +44,18 @@ public class AssignStoryCommand extends BaseCommand {
             }
         }
         throw new IllegalArgumentException(String.format("There is no story %s in team %s!", storyToReassign, memberTeam.getName()));
+    }
+
+    private static void throwIfInvalidAssignee(String assignee, Team teamOfLoggedInMember, List<Member> membersInTeam) {
+        boolean isMember = false;
+        for (Member member1 : membersInTeam) {
+            if(member1.getUsername().equals(assignee)) {
+                isMember=true;
+            }
+        }
+        if (!isMember) {
+            throw new IllegalArgumentException(String.format(ASSIGNEE_ERR_MESSAGE, assignee, teamOfLoggedInMember.getName()));
+        }
     }
 
     @Override
